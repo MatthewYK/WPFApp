46bd86606452d969f0a6805c8157c874c16ab551
Mateo Zuñiga <mateozuniga329@gmail.com>
2024-10-19 23:38:03 -0500

    Task 1-7 Final

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -20,10 +20,45 @@ namespace WPFApp
         {
             InitializeComponent();
         }
+        void AddMessage(string message)
+        {
+            int CurrentThreadId = Thread.CurrentThread.ManagedThreadId;
+            this.Dispatcher.Invoke(() =>
+            {
+            Messages.Content +=
+                $"Mensaje:{message]," +
+                $"Hilo actual: {Thread.CurrentThread.ManagedThreadId}";
+            });
+        }
 
         void CreateTask()
         {
             Task T;
+            Action Code = new Action(ShowMesage);
+            T= new Task (Code);
+            Task T2 = new Task(delegate
+            {
+                MessageBox.Show("Ejecutando una tarea  en un metodo anonimo.");
+            }
+                );
+            Task T3 = new Task(
+                () => ShowMesage());
+
+            Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+            Task T5 = new Task(() =>
+            {
+                DateTime CurrentDate = DateTime.Today;
+                DateTime StartDatec = CurrentDate.AddDays(30);
+                MessageBox.Show($"Tarea 5. Fecha Calculada : {StartDate}");
+            });
+
+            Task T6 = new Task((message) =>
+            MessageBox.Show(message.ToString()), "Expresion lambda con parametros.");
+
+            Task T7 = new Task(() => AddMessage("Ejecutando la tarea."));
+            T7.Start();
+            AddMessage("En el Hilo Principal");
         }
         void ShowMesage()
         {
